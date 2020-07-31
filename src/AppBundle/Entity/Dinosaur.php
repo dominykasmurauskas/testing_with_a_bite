7964c5bd4e8255f140ab7a923cf98945bebe5319
unknown <dominykasmurauskas1@gmail.com>
2020-07-31 14:09:18 +0300

    [E02] Dinosaur Getter/Setter test

---
+++ src/AppBundle/Entity/Dinosaur.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="dinosaurs")
+ */
+class Dinosaur
+{
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $length = 0;
+
+    public function getLength(): int
+    {
+        return $this->length;
+    }
+
+    public function setLength(int $length): self
+    {
+        $this->length = $length;
+
+        return $this;
+    }
+}
