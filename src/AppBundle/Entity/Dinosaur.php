ca6046822cfc252c3540439db3b0fbbe1cc6ab43
unknown <dominykasmurauskas1@gmail.com>
2020-07-31 15:01:26 +0300

    [E04] Dinosaur genus and carnivorousity getters/setters

--- src/AppBundle/Entity/Dinosaur.php
+++ src/AppBundle/Entity/Dinosaur.php
@@ -15,6 +15,22 @@ class Dinosaur
      */
     private $length = 0;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $genus;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isCarnivorous;
+
+    public function __construct(string $genus = 'Unknown', bool $isCarnivorous = false)
+    {
+        $this->genus = $genus;
+        $this->isCarnivorous = $isCarnivorous;
+    }
+
     public function getLength(): int
     {
         return $this->length;
@@ -23,6 +39,40 @@ class Dinosaur
     public function setLength(int $length): self
     {
         $this->length = $length;
+
+        return $this;
+    }
+
+    public function getSpecification(): string
+    {
+        return sprintf(
+            'The %s %scarnivorous dinosaur is %d meters long',
+            $this->genus,
+            $this->isCarnivorous ? '' : 'non-',
+            $this->length
+        );
+    }
+
+    public function getGenus(): string
+    {
+        return $this->genus;
+    }
+
+    public function setGenus(string $genus): self
+    {
+        $this->genus = $genus;
+
+        return $this;
+    }
+
+    public function isCarnivorous(): bool
+    {
+        return $this->isCarnivorous;
+    }
+
+    public function setIsCarnivorous(bool $isCarnivorous): self
+    {
+        $this->isCarnivorous = $isCarnivorous;
 
         return $this;
     }
--- tests/AppBundle/Entity/DinosaurTest.php
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -21,4 +21,24 @@ class DinosaurTest extends TestCase
         $dinosaur->setLength(15);
         $this->assertGreaterThan(12, $dinosaur->getLength());
     }
+
+    public function testReturnsFullSpecificiationOfDinosaur()
+    {
+        $dinosaur = new Dinosaur();
+
+        $this->assertSame(
+            'The Unknown non-carnivorous dinosaur is 0 meters long',
+            $dinosaur->getSpecification()
+        );
+    }
+
+    public function testReturnsFullSpecificationForTyrannosaurus()
+    {
+        $dinosaur = new Dinosaur('Tyrannosaurus', true);
+        $dinosaur->setLength(12);
+        $this->assertSame(
+            'The Tyrannosaurus carnivorous dinosaur is 12 meters long',
+            $dinosaur->getSpecification()
+        );
+    }
 }
