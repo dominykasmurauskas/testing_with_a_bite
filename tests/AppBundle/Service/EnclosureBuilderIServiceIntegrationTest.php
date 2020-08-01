1d73af35f6c51bd848f0032c55e4cab52da3185b
unknown <dominykasmurauskas1@gmail.com>
2020-08-01 20:10:16 +0300

    [E21] Clearing the database

--- tests/AppBundle/Service/EnclosureBuilderIServiceIntegrationTest.php
+++ tests/AppBundle/Service/EnclosureBuilderIServiceIntegrationTest.php
@@ -3,24 +3,30 @@
 namespace Tests\AppBundle\Service;
 
 use AppBundle\Entity\Dinosaur;
+use AppBundle\Entity\Enclosure;
 use AppBundle\Entity\Security;
 use AppBundle\Service\EnclosureBuilderService;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class EnclosureBuilderIServiceIntegrationTest extends KernelTestCase
 {
-    public function testItBuildsEnclosureWithDefaultSpecification()
+    protected function setUp()
     {
         self::bootKernel();
 
+        $this->truncateEntities();
+    }
+
+    public function testItBuildsEnclosureWithDefaultSpecification()
+    {
         /** @var EnclosureBuilderService $enclosureBuilderService */
         $enclosureBuilderService = self::$kernel->getContainer()
             ->get('test.'.EnclosureBuilderService::class);
 
         $enclosureBuilderService->buildEnclosure();
-        /** @var EntityManagerInterface $em */
-        $em = self::$kernel->getContainer()->get('doctrine')->getManager();
+        $em = $this->getEntityManager();
         $count = (int) $em->getRepository(Security::class)
             ->createQueryBuilder('s')
             ->select('COUNT(s.id)')
@@ -36,5 +42,16 @@ class EnclosureBuilderIServiceIntegrationTest extends KernelTestCase
             ->getSingleScalarResult();
 
         $this->assertSame(3, $count, 'Amount of dinosaurs is not the same');
+    }
+
+    private function truncateEntities()
+    {
+        $purger = new ORMPurger($this->getEntityManager());
+        $purger->purge();
+    }
+
+    private function getEntityManager(): EntityManagerInterface
+    {
+        return self::$kernel->getContainer()->get('doctrine')->getManager();
     }
 }
