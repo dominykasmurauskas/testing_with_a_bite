66c158d314f80450cb4ca6e21ce29ca11d548d0e
unknown <dominykasmurauskas1@gmail.com>
2020-08-01 13:05:02 +0300

    [E14] Control mock return value

--- tests/AppBundle/Factory/DinosaurFactoryTest.php
+++ tests/AppBundle/Factory/DinosaurFactoryTest.php
@@ -16,10 +16,15 @@ class DinosaurFactoryTest extends TestCase
      */
     private $factory;
 
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    private $lengthDeterminator;
+
     public function setUp()
     {
-        $mockLengthDeterminator = $this->createMock(DinosaurLengthDeterminator::class);
-        $this->factory = new DinosaurFactory($mockLengthDeterminator);
+        $this->lengthDeterminator = $this->createMock(DinosaurLengthDeterminator::class);
+        $this->factory = new DinosaurFactory($this->lengthDeterminator);
     }
 
     public function testItGrowsAVelociraptor()
@@ -52,9 +57,13 @@ class DinosaurFactoryTest extends TestCase
      */
     public function testItGrowsDinosaurFromASpecification(string $spec, bool $expectedIsCarnivorous)
     {
+        $this->lengthDeterminator->method('getLengthFromSpecification')
+            ->willReturn(20);
+
         $dinosaur = $this->factory->growFromSpecification($spec);
 
         $this->assertSame($expectedIsCarnivorous, $dinosaur->isCarnivorous(), 'Diets do not match');
+        $this->assertSame(20, $dinosaur->getLength());
     }
 
     public function getSpecificationTests()
